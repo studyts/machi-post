2905f5d91c18960b59bee6cceb1b689333176dba
yoshi <studyts744@gmail.com>
2019-04-12 08:47:15 +0000

    welcome-blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
                 <div class="topMain">
                     <div class="topMainBody">
                         <h1 class="site-title">MACHI-POST</h1>
-                        <p>ああああああの共有サイト</p>
+                        <p>都市の移り変わりを楽しむ共有サイト</p>
                         @if (Auth::check())
 	                        <a href="/users/{{$user->id}}" class="btn my-1">投稿はこちらから</a>
                         @else
